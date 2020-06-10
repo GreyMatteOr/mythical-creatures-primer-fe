a3d4bdf82354832d3fd26debab49323bd51e0bd1
Matthew Lane <themplane@gmail.com>
2020-06-09 18:17:29 -0700

    Fix Errors

--- yeti.js
+++ yeti.js
@@ -6,32 +6,37 @@ class Yeti {
     this.reindeer_eaten = ["Rudolph"]
   }
 
-  run_away() {
+  run_away(name) {
     this.weight -= 10;
     console.log(`We'll have to outwit the ${name} with our superior intelligence.`)
   }
 
   move_to_secret_location(locations){
-    this.home = locations[2].split("").reverse().join("")
+    this.home = this.home[2].split("").reverse().join("")
   }
 
   number_of_reindeer_eaten() {
-    this.reindeers_eaten.count
+    this.reindeer_eaten.length
   }
 
-  isHungry {
-    if (this.weight > 300) {
+  isHungry() {
+    if(this.weight > 300) {
       return false
-    } else {
+    }
+    else {
       return true
+    }
+  }
 
+  bounce(){
+    return "Boing Boing!"
   }
 }
 
-yeti = new Yeti("Bumble", "Colorado")
+yeti = new Yeti("Bumble", "Colorado", 400)
 yeti.number_of_reindeer_eaten()
 yeti.isHungry()
 yeti.bounce()
 yeti.move_to_secret_location(["California", "Oregon"])
-this.home()
-yeti.run_away()
+console.log(yeti.home);
+yeti.run_away('Bumble')
